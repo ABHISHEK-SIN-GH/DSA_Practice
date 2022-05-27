c9f38392ecad45ac1eabfeb49645cde16c7e6f4f
Abhishek <singh.abhishek151019@gmail.com>
2022-05-27 17:31:22 +0530

    dsa-practice

---
+++ String/printSubStr.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+string s = "abc";
+unordered_set<string> ss;
+for (int i = 0; i < s.size(); i++)
+{
+    for (int j = 1; j <= s.size()-i; j++)
+    {
+        cout<<s.substr(i,j)<<endl;
+    }
+}
+for (auto st: ss)
+{
+    cout<<st<<" ";
+}
+return 0;
+}
